e8ec21a400c1d8426fd1f36eaa6d60cd5e336cd8
Ivan <ivinci.design@gmail.com>
2021-07-02 08:26:38 -0700

    dotenv config

--- myApp.js
+++ myApp.js
@@ -1,7 +1,31 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
+console.log("Hello World")
 
+// Serve static assets
+app.use('/public', express.static(__dirname +'/public'))
+
+// Serve JSON on a Specific Route
+app.get('/json', (req, res) => {
+  // Send JSON response
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json(
+      { "message": "HELLO JSON" }
+    )
+  } else {
+    res.json(
+      { "message": "Hello json"}
+    )
+  }
+
+})
+
+// Serve an HTML File
+app.get('/', (req, res) => {
+  let absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath)
+})
 
 
 
